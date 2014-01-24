2e50acf2f6c6c1aef4d09a4787e56fe33b3131c0
Justin Kiggins <justin.kiggins@gmail.com>
2014-01-24 10:40:40 -0800

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='glab-common-py',
+      version='',
+      description='shared code for common lab functions, analyses, etc',
+      author='Justin Kiggins',
+      author_email='gward@python.net',
+      packages=['glab_common_py'],
+     )
