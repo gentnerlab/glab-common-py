53262e42dd9487030ef2c3282167b408c9d39cf6
Justin Kiggins <justin.kiggins@gmail.com>
2014-12-08 14:25:39 -0800

    added scripts for klustakwik

--- setup.py
+++ setup.py
@@ -2,10 +2,15 @@
 
 from distutils.core import setup
 
-setup(name='glab-common-py',
-      version='0.0.1',
-      description='shared code for common lab functions, analyses, etc',
-      author='Justin Kiggins',
-      author_email='justin.kiggins@gmail.com',
-      packages=['glab_common'],
-     )
+setup(
+    name='glab-common-py',
+    version='0.0.1',
+    description='shared code for common lab functions, analyses, etc',
+    author='Justin Kiggins',
+    author_email='justin.kiggins@gmail.com',
+    packages=['glab_common'],
+    scripts=[
+        'scripts/make_s2mat_list',
+        'scripts/s2mat_to_kwd',
+    ]
+)
