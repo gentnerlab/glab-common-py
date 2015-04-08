64bbfb43f0915a7a61082d7c31b905251f8c20cf
Justin Kiggins <justin.kiggins@gmail.com>
2015-04-08 11:10:25 -0700

    moved klustakwik helper scripts to own repo

--- setup.py
+++ setup.py
@@ -9,12 +9,6 @@ setup(
     author='Justin Kiggins',
     author_email='justin.kiggins@gmail.com',
     packages=['glab_common',
-              'kk_pipeline',
               ],
-    entry_points={
-        'console_scripts': [
-            's2mat_to_kwd = kk_pipeline.s2mat_to_kwd:main',
-            'make_s2mat_list = kk_pipeline.make_s2mat_list:main'
-        ],
     },
 )
