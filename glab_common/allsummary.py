1bc538c607ab04732972f231c688c980e436466d
Brad Theilman <bradtheilman@gmail.com>
2018-11-07 09:40:48 -0800

    Revert "New pandas"

--- glab_common/allsummary.py
+++ glab_common/allsummary.py
@@ -1,5 +1,4 @@
 #!/usr/local/bin/python
-import os
 import re
 import datetime as dt
 from behav.loading import load_data_pandas
@@ -8,115 +7,100 @@ from pyoperant.local import DATA_PATH
 
 process_fname = DATA_PATH + "panel_subject_behavior"
 
+inf = open(process_fname, 'rt')
+
 box_nums = []
 bird_nums = []
 processes = []
 
-# read in boxes, birds, and process information
-with open(process_fname, 'rt') as inf:
-    for line in inf.readlines():
-        # skip comment lines & blank lines
-        if line.strip() and not line.startswith("#"):
-            spl_line = line.split()
-            if spl_line[1] == "1":  # box enabled
-                box_nums.append(int(spl_line[0]))
-                bird_nums.append(int(spl_line[2]))
-                processes.append(spl_line[4])
+for line in inf.readlines():
+    if line.startswith('#') or not line.strip():
+        pass # skip comment lines & blank lines
+    else:
+        spl_line = line.split()
+        if spl_line[1] == "1": #box enabled
+            box_nums.append(int(spl_line[0]))
+            bird_nums.append(int(spl_line[2]))
+            processes.append(spl_line[4])
 
+inf.close()
+subjects = ['B%d' % (bird_num) for bird_num in bird_nums]
 data_folder = '/home/bird/opdat'
-
-write_lines = []
 
 f = open('/home/bird/all.summary', 'w')
 
 f.write("this all.summary generated at %s\n" % (dt.datetime.now().strftime('%x %X')))
+
 f.write("FeedErr(won't come up, won't go down, already up, resp during feed)\n")
 
 # Now loop through each bird and grab the error info from each summaryDAT file
 for (box, bird, proc) in zip(box_nums, bird_nums, processes):
+    try:
+        if proc in ('shape','lights','pylights', 'lights.py'):
+            f.write("box %d\tB%d\t %s\n" % (box, bird, proc))
+        else:
+            summaryfname = "/home/bird/opdat/B%d/%d.summaryDAT" % (bird, bird)
+            sdat = open(summaryfname, 'rt')
+            sdata = sdat.read()
+            sdat.close()
+            # m = re.search(r"Trials this session: (\w+)", sdata)
+            # trials_run = m.group(1)
+            # m = re.search(r"ops today: (\w+)", sdata)
+            # feeder_ops = m.group(1)
+            # m = re.search(r"d responses: (\w+)", sdata)
+            # rf_resp = m.group(1)
 
-    subj = 'B%d' % (bird)
+            m = re.search(r"failures today: (\w+)", sdata)
+            hopper_failures = m.group(1)
+            m = re.search(r"down failures today: (\w+)", sdata)
+            godown_failures = m.group(1)
+            m = re.search(r"up failures today: (\w+)", sdata)
+            goup_failures = m.group(1)
+            m = re.search(r"Responses during feed: (\w+)", sdata)
+            resp_feed = m.group(1)
+            # m = re.search(r"Last trial run @: (\w+)\s+(\w+)\s+(\d+)\s+(\d+)\:(\d+)\:(\d+)", sdata)
+            # last_trial_day = int(m.group(3))
+            # last_trial_hour = int(m.group(4))
+            # last_trial_min = int(m.group(5))
+            # last_trial_month = m.group(2)
 
-    box_bird_tag = "box %d\tB%d\t" % (box, bird)
+            # # Figure out time since last trial
+            # curr_time = datetime.now()
+            # if curr_time.day != last_trial_day:
+            #     datediff = '(not today)'
+            # else:
+            #     timediff = (curr_time.hour - last_trial_hour)*60 + (curr_time.minute - last_trial_min)
+            #     datediff = '(%d mins ago)' % timediff
+            
+            # calculate output fields not written directly
+            #TOs = int(rf_resp) - int(feeder_ops)
+            #noRs = int(trials_run) - int(rf_resp)
+            
 
-    if proc in ('shape', 'lights', 'pylights', 'lights.py'):
-        write_lines.append("%s %s" % (box_bird_tag, proc))
-        continue
 
-    summaryfname = "/home/bird/opdat/%s/%d.summaryDAT" % (subj, bird)
+            subj = 'B%d' % (bird)
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore")
+                behav_data = load_data_pandas([subj], data_folder);
+            df = behav_data[subj]
+            #df = df[~pd.isnull(data.index)]
+            todays_data = df[(df.index.date-dt.datetime.today().date()) == dt.timedelta(days=0)]
+            feeder_ops = sum(todays_data['reward'].values)
+            trials_run = len(todays_data)
+            noRs = sum(todays_data['response'].values=='none')
+            TOs = trials_run-feeder_ops-noRs
+            last_trial_time = todays_data.sort_index().tail().index[-1]
+            if last_trial_time.day != dt.datetime.now().day:
+                datediff = '(not today)'
+            else:
+                minutes_ago = (dt.datetime.now() - last_trial_time).seconds / 60
+                datediff = '(%d mins ago)' % (minutes_ago)
+            
+            outline = "box %d\tB%d\t %s  \ttrls=%s  \tfeeds=%d  \tTOs=%d  \tnoRs=%d  \tFeedErrs=(%s,%s,%s,%s)  \tlast @ %s %s\n" % (box, bird, proc, trials_run, feeder_ops, TOs, noRs, 
+              hopper_failures, godown_failures, goup_failures, resp_feed, last_trial_time.strftime('%x %X'), datediff)
+            f.write(outline)
+    except Exception as e:
+        f.write("box %d\tB%d\t Error opening SummaryDat or incorrect format\n" % (box, bird))
+        print e
 
-    if not os.path.isfile(summaryfname):
-        write_lines.append("%s SummaryDat does not exist" % (box_bird_tag))
-        continue
-
-    sdata = ""
-    with open(summaryfname, 'rt') as f:
-        sdata = f.read()
-
-    # m = re.search(r"Trials this session: (\w+)", sdata)
-    # trials_run = m.group(1)
-    # m = re.search(r"ops today: (\w+)", sdata)
-    # feeder_ops = m.group(1)
-    # m = re.search(r"d responses: (\w+)", sdata)
-    # rf_resp = m.group(1)
-
-    m = re.search(r"failures today: (\w+)", sdata)
-    hopper_failures = m.group(1)
-    m = re.search(r"down failures today: (\w+)", sdata)
-    godown_failures = m.group(1)
-    m = re.search(r"up failures today: (\w+)", sdata)
-    goup_failures = m.group(1)
-    m = re.search(r"Responses during feed: (\w+)", sdata)
-    resp_feed = m.group(1)
-
-    # m = re.search(r"Last trial run @: (\w+)\s+(\w+)\s+(\d+)\s+(\d+)\:(\d+)\:(\d+)", sdata)
-    # last_trial_day = int(m.group(3))
-    # last_trial_hour = int(m.group(4))
-    # last_trial_min = int(m.group(5))
-    # last_trial_month = m.group(2)
-
-    # # Figure out time since last trial
-    # curr_time = datetime.now()
-    # if curr_time.day != last_trial_day:
-    #     datediff = '(not today)'
-    # else:
-    #     timediff = (curr_time.hour - last_trial_hour)*60 + (curr_time.minute - last_trial_min)
-    #     datediff = '(%d mins ago)' % timediff
-
-    # calculate output fields not written directly
-    # TOs = int(rf_resp) - int(feeder_ops)
-    # noRs = int(trials_run) - int(rf_resp)
-
-    if hopper_failures is None or godown_failures is None or goup_failures is None or resp_feed is None:
-        write_lines.append("%s Error reading SummaryDat, no hopper, go down, go up, or feeding response found" %
-                           (box_bird_tag))
-        continue
-
-    with warnings.catch_warnings():
-        warnings.simplefilter("ignore")
-        behav_data = load_data_pandas([subj], data_folder)
-
-    df = behav_data[subj]
-    # df = df[~pd.isnull(data.index)]
-    todays_data = df[(df.index.date - dt.datetime.today().date()) == dt.timedelta(days=0)]
-    feeder_ops = sum(todays_data['reward'].values)
-    trials_run = len(todays_data)
-    noRs = sum(todays_data['response'].values == 'none')
-    TOs = trials_run - feeder_ops - noRs
-    last_trial_time = todays_data.sort_index().tail().index[-1]
-
-    if last_trial_time.day != dt.datetime.now().day:
-        datediff = '(not today)'
-    else:
-        minutes_ago = (dt.datetime.now() - last_trial_time).seconds / 60
-        datediff = '(%d mins ago)' % (minutes_ago)
-
-    outline = "%s %s  \ttrls=%s  \tfeeds=%d  \tTOs=%d  \tnoRs=%d  \tFeedErrs=(%s,%s,%s,%s)  \tlast @ %s %s" % (
-    box_bird_tag, proc, trials_run, feeder_ops, TOs, noRs, hopper_failures, godown_failures, goup_failures, resp_feed,
-    last_trial_time.strftime('%x %X'), datediff)
-
-    write_lines.append(outline)
-
-write_lines = map(lambda x: x + '\n', write_lines)
-f.writelines(write_lines)
 f.close()
--- glab_common/compassion.py
+++ glab_common/compassion.py
@@ -1,11 +1,9 @@
 #!/usr/local/bin/python
 import re
 import datetime as dt
-import os
 from behav.loading import load_data_pandas
 from socket import gethostname
 import warnings
-
 try:
     import simplejson as json
 except ImportError:
@@ -13,102 +11,83 @@ except ImportError:
 
 from pyoperant.local import DATA_PATH
 
-process_fname = os.path.join(DATA_PATH, "panel_subject_behavior")
+process_fname = DATA_PATH + "panel_subject_behavior"
 
 box_nums = []
 bird_nums = []
 processes = []
 
-# read in boxes, birds, and process information
-with open(process_fname, 'rt') as f:
-    for line in f.readlines():
-        # skip comment lines & blank lines
-        if line.strip() and not line.startswith("#"):
-            split_line = line.split()
-            if split_line[1] == "1":  # box enabled
-                box_nums.append(int(split_line[0]))
-                bird_nums.append(int(split_line[2]))
-                processes.append(split_line[-1])
+with open(process_fname, 'rt') as in_f:
+    for line in in_f.readlines():
+        if line.startswith('#') or not line.strip():
+            pass # skip comment lines & blank lines
+        else:
+            spl_line = line.split()
+            if spl_line[1] == "1": #box enabled
+                box_nums.append(int(spl_line[0]))
+                bird_nums.append(int(spl_line[2]))
+                processes.append(spl_line[-1])
 
+subjects = ['B%d' % (bird_num) for bird_num in bird_nums]
 data_folder = '/home/bird/opdat'
 
-write_lines = []
+with open(DATA_PATH+'all.compassion', 'w') as f:
 
-f = open(os.path.join(DATA_PATH, 'all.compassion'), 'w')
+    f.write("this all.compassion ran on %s at %s\n" % (gethostname(), dt.datetime.now().strftime('%x %X')))
 
-f.write("this all.compassion ran on %s at %s\n" % (gethostname(), dt.datetime.now().strftime('%x %X')))
+    # Now loop through each bird and grab the error info from each summaryDAT file
+    for (box, bird, proc) in zip(box_nums, bird_nums, processes):
+        try:
+            if proc in ('Lights',):
+                pass
+            else:
+                configfname = "/home/bird/opdat/B%d/config.json" % (bird)
+                try:
+                    with open(configfname, 'rb') as config:
+                        parameters = json.load(config)
+                except IOError:
+                    parameters = ()
 
-# Now loop through each bird and grab the error info from each summaryDAT file
-for (box, bird, proc) in zip(box_nums, bird_nums, processes):
+                sched = parameters['light_schedule']
+                bird_owner = parameters['experimenter']['name']
+                bird_owner_email = parameters['experimenter']['email']
+                shaping = parameters['shape'] if 'shape' in parameters else False
 
-    if proc in ('Lights',):
-        continue
+                if shaping:
+                    f.write("Bird B%d\tBox %d\tOwner %s: Is on shape, hopefully %s is really watching them\n" % (bird, box, bird_owner, bird_owner))
+                else:
+                    subj = 'B%d' % (bird)
+                    with warnings.catch_warnings():
+                        warnings.simplefilter("ignore")
+                        behav_data = load_data_pandas([subj], data_folder);
+                    df = behav_data[subj]
+                    todays_data = df[(df.index.date-dt.datetime.today().date()) == dt.timedelta(days=0)]
+                    feeder_ops = sum(todays_data['reward'].values)
 
-    subj = 'B%d' % (bird)
-    box_bird_tag = "box %d\t%s\t" % (box, subj)
+                    (latitude, longitude) = ('32.82', '-117.14') # San Diego, CA
+                    fmt="%H:%M"
+                    if sched == 'sun':
+                        import ephem
+                        obs = ephem.Observer()
+                        obs.lat = latitude # San Diego, CA
+                        obs.long = longitude
+                        sun = ephem.Sun()
+                        sun.compute()
+                        start = ephem.localtime(obs.previous_rising(sun))
+                        end = ephem.localtime(obs.next_setting(sun))
+                    else:
+                        if len(sched) != 1:
+                            raise Exception("someone is using non standard schedules and has to fix this script themselves")
+                        start = dt.datetime.combine(dt.date.today(), dt.datetime.time(dt.datetime.strptime(sched[0][0], fmt)))
+                        end = dt.datetime.combine(dt.date.today(), dt.datetime.time(dt.datetime.strptime(sched[0][1], fmt)))
 
-    configfname = "/home/bird/opdat/%s/config.json" % (subj)
-
-    if os.path.isfile(configfname):
-        with open(configfname, 'rb') as config:
-            parameters = json.load(config)
-    else:
-        parameters = ()
-
-    try:
-        sched = parameters['light_schedule']
-        bird_owner = parameters['experimenter']['name']
-        bird_owner_email = parameters['experimenter']['email']
-        shaping = parameters['shape'] if 'shape' in parameters else False
-    except Exception as e:
-        write_lines.append("%s Error reading config.json: %e" % (box_bird_tag, repr(e)))
-        continue
-
-    if shaping:
-        write_lines.append("%s Owner %s: Is on shape, hopefully %s is really watching them" % (
-            box_bird_tag, bird_owner, bird_owner))
-        continue
-
-    with warnings.catch_warnings():
-        warnings.simplefilter("ignore")
-        behav_data = load_data_pandas([subj], data_folder)
-
-    df = behav_data[subj]
-    todays_data = df[(df.index.date - dt.datetime.today().date()) == dt.timedelta(days=0)]
-    feeder_ops = sum(todays_data['reward'].values)
-
-    (latitude, longitude) = ('32.82', '-117.14')  # San Diego, CA
-    fmt = "%H:%M"
-
-    # initialize the feeding schedule
-    if sched == 'sun':
-        import ephem
-
-        obs = ephem.Observer()
-        obs.lat = latitude  # San Diego, CA
-        obs.long = longitude
-        sun = ephem.Sun()
-        sun.compute()
-        start = ephem.localtime(obs.previous_rising(sun))
-        end = ephem.localtime(obs.next_setting(sun))
-    else:
-        if len(sched) != 1:
-            write_lines.append("%s Error: non standard schedules, fix this script" % (box_bird_tag))
-            continue
-
-        start = dt.datetime.combine(dt.date.today(),
-                                    dt.datetime.time(dt.datetime.strptime(sched[0][0], fmt)))
-        end = dt.datetime.combine(dt.date.today(),
-                                  dt.datetime.time(dt.datetime.strptime(sched[0][1], fmt)))
-
-    duration = end - start
-    expected_feeds = 100 * (dt.datetime.now() - start).total_seconds() / duration.total_seconds()
-    expected_feeds = min(expected_feeds, 100)
-
-    if feeder_ops < expected_feeds:
-        write_lines.append("%s Owner %s: Feeds are insufficient. Expected: %d Actual: %d" % (
-            box_bird_tag, bird_owner, expected_feeds, feeder_ops))
-
-write_lines = map(lambda x: x + '\n', write_lines)
-f.writelines(write_lines)
-f.close()
+                    duration = end - start
+                    expected_feeds = 100 * (dt.datetime.now() - start).total_seconds() / duration.total_seconds()
+                    expected_feeds = min(expected_feeds, 100)
+                    
+                    if feeder_ops < expected_feeds:
+                        f.write("Bird B%d\tBox %d\tOwner %s: Feeds are insufficient. Expected: %d Actual: %d\n" % (bird, box, bird_owner, expected_feeds, feeder_ops))
+               
+        except Exception as e:
+            f.write("box %d\tB%d\t Error opening SummaryDat or incorrect format\n" % (box, bird))
+            print e, box, bird, proc
