8228be5cd67567f4c4ff2449274a3f48f601aed5
Marvin T <MarvinT@users.noreply.github.com>
2018-11-08 17:30:31 -0500

    PEP8

--- glab_common/utils.py
+++ glab_common/utils.py
@@ -6,6 +6,7 @@ import datetime as dt
 import os
 import matplotlib.pyplot as plt
 
+
 def load_mat(filename):
     '''
     this function should be called instead of direct spio.loadmat
@@ -25,6 +26,7 @@ def load_mat(filename):
     data = sio.loadmat(filename, struct_as_record=False, squeeze_me=True)
     return _check_keys(data)
 
+
 def _check_keys(dict):
     '''
     checks if entries in dictionary are mat-objects. If yes
@@ -34,6 +36,7 @@ def _check_keys(dict):
         if isinstance(dict[key], sio.matlab.mio5_params.mat_struct):
             dict[key] = _todict(dict[key])
     return dict
+
 
 def _todict(matobj):
     '''
@@ -48,29 +51,31 @@ def _todict(matobj):
             dict[strg] = elem
     return dict
 
-def load_rDAT(fin,nheaderrows = 0,fmt=None):
-    if fmt == None: #replace with your own rdat format
-        fmt = [('session','i4'),
-               ('trial','i4'),
-               ('normal','b'),
-               ('stimulus','a64'),
-               ('class','i4'),
-               ('R_sel','i4'),
-               ('R_acc','i4'),
-               ('ReactionTime','f4'),
-               ('Reinforced','b'),
-               ('TimeOfDay','a8'),
-               ('Date','a8'),
-               ];
+
+def load_rDAT(fin, nheaderrows=0, fmt=None):
+    if fmt == None:  # replace with your own rdat format
+        fmt = [('session', 'i4'),
+               ('trial', 'i4'),
+               ('normal', 'b'),
+               ('stimulus', 'a64'),
+               ('class', 'i4'),
+               ('R_sel', 'i4'),
+               ('R_acc', 'i4'),
+               ('ReactionTime', 'f4'),
+               ('Reinforced', 'b'),
+               ('TimeOfDay', 'a8'),
+               ('Date', 'a8'),
+               ]
 
     while True:
         if nheaderrows > 100:
             raise ValueError('Recursively found more than 100 header rows.')
         try:
-            data = np.genfromtxt(fin,dtype=fmt,invalid_raise=False,skip_header=nheaderrows)
+            data = np.genfromtxt(fin, dtype=fmt, invalid_raise=False, skip_header=nheaderrows)
             return data
         except ValueError:
             nheaderrows += 1
+
 
 def load_data_pandas(subjects, data_folder, force_boolean=['reward']):
     '''
@@ -78,11 +83,13 @@ def load_data_pandas(subjects, data_folder, force_boolean=['reward']):
     '''
     raise DeprecationWarning('test')
 
-def binomial_ci(x,N,CL=95.0):
+
+def binomial_ci(x, N, CL=95.0):
     '''
     This function is deprecated and has been moved to Gentnerlab/behav-analysis
     '''
     raise DeprecationWarning('test')
+
 
 def vinjegallant(response):
     '''
@@ -101,14 +108,13 @@ def vinjegallant(response):
     n = np.float_(len(R))
     eps = np.spacing(np.float64(1))
 
-    A = ((R.sum()/n)**2) / (((R**2).sum()/n) + eps)
-    S = (1 - A) / (1 - 1/n)
+    A = ((R.sum() / n)**2) / (((R**2).sum() / n) + eps)
+    S = (1 - A) / (1 - 1 / n)
 
     return S
 
 
-
-def accperstimplot(subj,df,days=7,stims_all=None):
+def accperstimplot(subj, df, days=7, stims_all=None):
     '''
     This function is deprecated and has been moved to Gentnerlab/behav-analysis
     '''
@@ -121,13 +127,15 @@ def stars(p):
     '''
     raise DeprecationWarning('test')
 
-def plot_stars(p,x,y,size='large',horizontalalignment='center',**kwargs):
+
+def plot_stars(p, x, y, size='large', horizontalalignment='center', **kwargs):
     '''
     This function is deprecated and has been moved to Gentnerlab/behav-analysis
     '''
     raise DeprecationWarning('test')
 
-def plot_linestar(p,x1,x2,y):
+
+def plot_linestar(p, x1, x2, y):
     '''
     This function is deprecated and has been moved to Gentnerlab/behav-analysis
     '''
